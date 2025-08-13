c8e2d0067ddb880f3990f4fd31c2a32cace6be6e
mohammed shafeeq <shafeeqnez401@gmail.com>
2025-08-13 14:07:55 +0530

    Time: 30 ms (84.07%), Space: 47.2 MB (20.54%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int minCut(String s) {
+        int n = s.length();
+        boolean[][] isPal = new boolean[n][n];
+
+        // Step 1: Precompute palindrome substrings
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j) && (j - i < 2 || isPal[i + 1][j - 1])) {
+                    isPal[i][j] = true;
+                }
+            }
+        }
+
+        // Step 2: DP for min cut
+        int[] dp = new int[n];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+
+        for (int i = 0; i < n; i++) {
+            if (isPal[0][i]) {
+                dp[i] = 0;
+            } else {
+                for (int j = 1; j <= i; j++) {
+                    if (isPal[j][i]) {
+                        dp[i] = Math.min(dp[i], dp[j - 1] + 1);
+                    }
+                }
+            }
+        }
+
+        return dp[n - 1];
+    }
+}
